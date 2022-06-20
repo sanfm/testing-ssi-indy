d7c423f8c9064168c517cc16fb843e72f17db282
sanfm <fede22ms@gmail.com>
2022-06-20 22:26:08 +0200

    update test agent aiohttp

--- aries-agents/UGR_v1.py
+++ aries-agents/UGR_v1.py
@@ -4,20 +4,22 @@ import asyncio
 
 async def main():
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(admin_api + "/connections/create-invitation") as response:
+    # a ClientSession() le podemos añadir una URL_base que podemos usar
+    # en todas las peticiones posteriores: clientSession(Base_URL)
+    async with aiohttp.ClientSession('http://0.0.0.0:11000') as session:
+        async with session.post('/connections/create-invitation') as response:
 
             print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+            #print("Content-type:", response.headers['content-type'])
 
-            resp = await response.text()
+            resp = await response.json()
             #print("Body:", html[:15], "...")
             print(resp)
 
 
 
 
-admin_api = 'http://0.0.0.0:11000'
+#admin_api = 'http://0.0.0.0:11000'
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
